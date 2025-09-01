06c443e720652ab5a1fe309c610a98ef117c5f1b
somalang <sohyun@ati2000.co.kr>
2025-09-01 14:34:28 +0900

    feat : add menubar at top

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using Microsoft.Win32;
 using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Media.Imaging;
 
 namespace ImageProcessing.ViewModels
@@ -10,6 +11,7 @@ namespace ImageProcessing.ViewModels
     public class MainViewModel : INotifyPropertyChanged
     {
         private BitmapImage _loadedImage;
+        private string _lastFilePath;
 
         public BitmapImage LoadedImage
         {
@@ -24,17 +26,59 @@ namespace ImageProcessing.ViewModels
 
         public bool HasImage => LoadedImage != null;
 
+        // 파일 관련 명령
         public RelayCommand LoadImageCommand { get; }
         public RelayCommand DeleteImageCommand { get; }
         public RelayCommand ReloadImageCommand { get; }
+        public RelayCommand SaveImageCommand { get; }
+        public RelayCommand ExitCommand { get; }
 
-        private string _lastFilePath;
+        // 편집 관련
+        public RelayCommand UndoCommand { get; }
+        public RelayCommand RedoCommand { get; }
+
+        // 필터
+        public RelayCommand GrayscaleCommand { get; }
+        public RelayCommand GaussianBlurCommand { get; }
+        public RelayCommand MedianFilterCommand { get; }
+
+        // FFT
+        public RelayCommand FFTCommand { get; }
+        public RelayCommand IFFTCommand { get; }
+
+        // 매칭
+        public RelayCommand TemplateMatchCommand { get; }
+
+        // 설정
+        public RelayCommand OpenSettingsCommand { get; }
 
         public MainViewModel()
         {
+            // 파일
             LoadImageCommand = new RelayCommand(_ => LoadImage());
             DeleteImageCommand = new RelayCommand(_ => DeleteImage(), _ => HasImage);
             ReloadImageCommand = new RelayCommand(_ => ReloadImage(), _ => !string.IsNullOrEmpty(_lastFilePath));
+            SaveImageCommand = new RelayCommand(_ => SaveImage(), _ => HasImage);
+            ExitCommand = new RelayCommand(_ => Application.Current.Shutdown());
+
+            // 편집 (Undo/Redo는 스택 구현 필요, 여기선 비워둠)
+            UndoCommand = new RelayCommand(_ => MessageBox.Show("되돌리기 실행"));
+            RedoCommand = new RelayCommand(_ => MessageBox.Show("다시하기 실행"));
+
+            // 필터
+            GrayscaleCommand = new RelayCommand(_ => MessageBox.Show("그레이스케일 실행"));
+            GaussianBlurCommand = new RelayCommand(_ => MessageBox.Show("가우시안 블러 실행"));
+            MedianFilterCommand = new RelayCommand(_ => MessageBox.Show("미디언 필터 실행"));
+
+            // FFT
+            FFTCommand = new RelayCommand(_ => MessageBox.Show("FFT 실행"));
+            IFFTCommand = new RelayCommand(_ => MessageBox.Show("역 FFT 실행"));
+
+            // 매칭
+            TemplateMatchCommand = new RelayCommand(_ => MessageBox.Show("템플릿 매칭 실행"));
+
+            // 설정
+            OpenSettingsCommand = new RelayCommand(_ => MessageBox.Show("환경 설정 창 열기"));
         }
 
         private void LoadImage()
@@ -68,6 +112,20 @@ namespace ImageProcessing.ViewModels
             bitmap.EndInit();
 
             LoadedImage = bitmap;
+        }
+
+        private void SaveImage()
+        {
+            var dialog = new SaveFileDialog
+            {
+                Filter = "PNG|*.png|JPEG|*.jpg|Bitmap|*.bmp"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                MessageBox.Show($"이미지를 {dialog.FileName} 으로 저장");
+                // TODO: ImageProcessor.SaveBitmap(LoadedImage, dialog.FileName);
+            }
         }
 
         private void DeleteImage()
